2d075fcbfa1ef305cf59ef8176a51bec53267ae2
774655048@qq.com <774655048@qq.com>
2017-06-10 13:47:28 +0800

    add functional_swift_optional

--- 可选值/functional_swift_optional.playground/Contents.swift
+++ 可选值/functional_swift_optional.playground/Contents.swift
@@ -1,5 +1,53 @@
 //: Playground - noun: a place where people can play
+//: Playground - 可选值
 
 import UIKit
 
-var str = "Hello, playground"
+
+let cities = ["Paris" : 2241, "Madrid" : 3165, "Amsterdam":827, "Berlin":3562]
+
+//可支持泛型
+let cities1: Dictionary<String, Int> = ["Paris" : 2241]
+let cities2: [String : Int] = ["Paris" : 2241]
+
+
+let madridPopulation: Int? = cities["Madrid"]
+
+//验证 --- unsafe!
+if madridPopulation != nil {
+    
+    print("存在Madrid")
+    
+} else {
+    
+    print("不存在Madrid")
+}
+
+
+if let _ = cities["Madrid"] {
+    
+    print("存在Madrid")
+    
+} else {
+    
+   print("不存在Madrid")
+    
+}
+
+
+infix operator ??
+
+func ??<T>(optional:T?, defaultValue:T) -> T {
+    
+    if let x = optional {
+        
+        return x
+        
+    } else {
+        
+        return defaultValue 
+    }
+}
+
+
+
