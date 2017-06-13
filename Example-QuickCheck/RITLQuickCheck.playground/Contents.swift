b058e9d787393cf00362570e2702dac7306c78bd
774655048@qq.com <774655048@qq.com>
2017-06-13 14:02:04 +0800

    add some function and add protocol

--- Example-QuickCheck/RITLQuickCheck.playground/Contents.swift
+++ Example-QuickCheck/RITLQuickCheck.playground/Contents.swift
@@ -52,8 +52,6 @@ protocol Smaller {
     func smaller() -> Self?
 }
 
-
-
 //构建生成随机数
 protocol Arbitrary : Smaller {
     
@@ -67,7 +65,14 @@ extension Int : Arbitrary {
         return Int(arc4random())
     }
     
+    func smaller() -> Int? {
+        
+        return self == 0 ? nil : self / 2
+    }
 }
+
+
+
 
 let arcRandom = Int.arbitrary()
 
@@ -90,6 +95,11 @@ extension UnicodeScalar : Arbitrary {
         
         return UnicodeScalar(Int.arbitrary(in: 65..<90))!
     }
+    
+    func smaller() -> UnicodeScalar? {
+        
+        return self
+    }
 }
 
 
@@ -106,6 +116,11 @@ extension String : Arbitrary {
         
         return String(UnicodeScalarView(randomScalars))//拼接字符串
     }
+    
+    func smaller() -> String? {
+        
+        return isEmpty ? nil : String(characters.dropFirst())
+    }
 }
 
 
@@ -115,9 +130,9 @@ let _ = String.arbitrary()
 
 ///MARK: 实现check方法
 
+let numberOfIterations = 10
+
 func check1<A:Arbitrary>(_ message:String,_ property:(A) -> Bool) -> () {
-    
-    let numberOfIterations = message.characters.count
     
     for _ in 0..<numberOfIterations {
         
@@ -130,6 +145,8 @@ func check1<A:Arbitrary>(_ message:String,_ property:(A) -> Bool) -> () {
     print("\"\(message)\" passed \(numberOfIterations) tests.")
 }
 
+
+
 //测试上述方法
 extension CGSize {
     
@@ -138,50 +155,117 @@ extension CGSize {
     }
 }
 
+
+
 extension CGSize : Arbitrary {
     
     static func arbitrary() -> CGSize {
         
         return CGSize(width: .arbitrary(), height: .arbitrary())
     }
+    
+    
+    func smaller() -> CGSize? {//无用
+        
+        return self
+    }
 }
+
+
 
 check1("Area should be at least 0"){ (size: CGSize) in size.area >= 0 }
 
-//缩小范围
+
 check1("Every string starts with Hello"){ (s: String) in
     s.hasPrefix("Hello")
 }
 
+///MARK: 缩小范围引用Smaller - 对于整数，尝试除以二，直到等于0
 
-extension Int: Smaller {
-    
-    func smaller() -> Int? {
-        
-        return self == 0 ? nil : self / 2
-    }
-}
+
 
 let _ = 100.smaller() //50
 
 
-//对于字符串，移除第一个字符(除非该字符串为空)
 
-extension String: Smaller {
+///MARK: 反复缩小范围
+
+
+/// 通过反复缩小测试中发现的范例所属的范围
+func iterate<A>(while condition:(A) -> Bool, initial: A, next:(A) -> A?) -> A {
     
-    func smaller() -> String? {
+    guard let x = next(initial),condition(x) else {// 没有操作返回初始值
         
-        return isEmpty ? nil : String(characters.dropFirst())
+        return initial
+    }
+    
+    return iterate(while: condition, initial: x, next: next)
+    
+}
+
+
+/// 随机整数
+func check2<A:Arbitrary>(_ message:String, _ property:(A) -> Bool) -> () {
+    
+    for _ in 0..<numberOfIterations {
+        
+        let value = A.arbitrary()
+        guard property(value) else {
+            
+            let smallerValue = iterate(while:{ !property( $0 ) },initial:value){ $0.smaller() }
+            
+            print("\"\(message)\" dosen't hold:\(smallerValue)"); return
+        }
+    }
+    
+    print("\"\(message)\" passed \(numberOfIterations) tests.")
+}
+
+
+/// 随机数组
+
+/// 函数式的快速排序
+func qsqrt(_ input:[Int]) -> [Int]
+{
+    var array = input
+    if array.isEmpty { return [] }//空判断
+    
+    let pivot = array.removeFirst() //第一个元素，并且移除
+    let lesser = array.filter{ $0 < pivot } // 获得所有比第一个元素小的数组
+    let greater = array.filter{ $0 >= pivot } // 获得所有大于等于第一个元素的数组
+    let intermediate = qsqrt(lesser) + [pivot] // 对小于的元素进行快排并添加最大的元素
+    
+    return intermediate + qsqrt(greater) //同理
+}
+
+
+
+extension Array : Smaller {
+    
+    func smaller() -> Array<Element>? {
+        
+        guard !isEmpty else { return nil }
+        return Array(dropLast())
     }
 }
 
 
-// 使用函数式的风格优化上述写法,使用map或者reduce替代for
+
+extension Array where Element: Arbitrary {
+    
+    static func arbitrary() -> [Element] {
+        
+        let randomLength = Int.arbitrary(in: 0..<50)
+        return (0..<randomLength).map{ _ in .arbitrary() }
+    }
+}
 
 
-
-
-
+///检测当前快排
+//check2("qsort should behave like sort"){ (x:[Int]) in
+//    
+//    return qsqrt(x) == x.sorted()
+//}
 
 
 
